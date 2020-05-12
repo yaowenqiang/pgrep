81844ac193b5a6eb7db8c1b6773d25fdfb60275a
yaowenqiang <yaowenqiang111@163.com>
2020-05-13 01:14:44 +0800

    add failure trait demo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use clap::{clap_app, crate_version};
 use regex::Regex;
 use std::path::Path;
+use failure::{Error, Fail};
+use std::fmt;
+
 #[derive(Debug)]
 struct Record {
     line: usize,
     tx:String,
 }
-fn main() -> Result<(), String>{
+
+#[derive(Debug)]
+struct ArgErr {
+    arg: &'static str,
+}
+
+impl Fail for ArgErr { }
+
+impl fmt::Display for ArgErr {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Argument Not provided!{}", self.arg)
+    }
+}
+
+
+fn main() -> Result<(), Error> {
     let cp = clap_app!{
         pgrep => 
             (version: crate_version!())
@@ -16,19 +34,21 @@ fn main() -> Result<(), String>{
             (@arg pattern : +required "The regexpattern to search for")
     }.get_matches();
 
-    let re = Regex::new( cp.value_of("pattern").unwrap()).map_err(|_| "bad regex")?;
+    let re = Regex::new( cp.value_of("pattern").unwrap())?;
+    //let p = process_file(
     let p = process_file(
-        cp.value_of("file").ok_or("No file chosen")?, 
-        re
+        //cp.value_of("file").ok_or("No file chosen")?, 
+        cp.value_of("file").ok_or(ArgErr {arg: "file"})?, 
+        &re
     );
     println!("{:?}", p);
     Ok(())
 }
 
-fn process_file<P:AsRef<Path>>(p:P, re: Regex) -> Result<Vec<Record>, String>
+fn process_file<P:AsRef<Path>>(p:P, re: &Regex) -> Result<Vec<Record>, Error>
 {
     let mut res = Vec::new();
-    let bts = std::fs::read(p).map_err(|e| "Could not read string".to_string())?;
+    let bts = std::fs::read(p)?;
     if let Ok(ss) = String::from_utf8(bts) {
         for (i, l) in ss.lines().enumerate() {
             if re.is_match(l) {
@@ -43,3 +63,33 @@ fn process_file<P:AsRef<Path>>(p:P, re: Regex) -> Result<Vec<Record>, String>
     };
     Ok(res)
 }
+
+/*
+fn process_path<P, FF, EF>(p:P, re:&Regex, ff:&FF, ef:&EF) -> Result<(), Error> 
+where 
+    P:AsRef<Path>, 
+    FF:Fn(&Path, Vec<Record>),
+    EF: Fn(Error),
+{
+    let p = p.as_ref();
+    let md = p.metadata()?;
+    let ft = md.file_type();
+    if ft.is_file() {
+        let dt =  process_file(p ,re)?;
+        ff(p, dt);
+    }
+
+    if ft.is_dir() {
+        let dd = std::fs::read_dir(p)?;
+
+        for d in dd {
+            if let Err(e) = process_path(d?.path(), re, ff) {
+                ef(e);
+            }
+
+        }
+    }
+    Ok(())
+
+}
+*/
